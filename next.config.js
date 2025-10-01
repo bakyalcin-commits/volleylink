81e51f5be30bcf28dfe4d84e5b4f54960ee3d48e
bakyalcin-commits <b.akyalcin@gmail.com>
2025-10-01 21:30:23 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: false,
+    // ffmpeg paketi bundle’a gömülmesin; Node runtime’da çöz
     serverComponentsExternalPackages: ['@ffmpeg-installer/ffmpeg'],
   },
 };
+
 module.exports = nextConfig;
