1e3b2a1b37f74dd8c4247d494831e25899920582
bakyalcin-commits <b.akyalcin@gmail.com>
2025-10-01 22:50:51 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: false,
-    serverComponentsExternalPackages: ['@ffmpeg-installer/ffmpeg'],
+  experimental: { typedRoutes: false },
+  webpack: (config) => {
+    // ffmpeg-static'ı runtime'da require edeceğiz; bundla gömmeyelim
+    config.externals = config.externals || [];
+    config.externals.push({
+      "ffmpeg-static": "commonjs ffmpeg-static",
+    });
+    return config;
   },
 };
+
 module.exports = nextConfig;
